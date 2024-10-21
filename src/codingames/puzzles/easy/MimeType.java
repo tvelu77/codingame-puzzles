25833fa8b8990d8bf03e7bc4c4709a06443b6560
Thomas VELU <velu.thomas77@laposte.net>
2024-10-21 21:05:56 +0200

    fix : format

--- src/codingames/puzzles/easy/MimeType.java
+++ src/codingames/puzzles/easy/MimeType.java
@@ -8,26 +8,26 @@ import java.util.Scanner;
  */
 public class MimeType {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int Q = in.nextInt();
-        HashMap<String, String> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            String EXT = in.next(); // file extension
-            String MT = in.next(); // MIME type.
-            map.putIfAbsent(EXT.toLowerCase(), MT);
-        }
-        in.nextLine();
-        for (int i = 0; i < Q; i++) {
-            String FNAME = in.nextLine(); // One file name per line.
-            String extension = "";
-            int dotIndex = FNAME.lastIndexOf(".");
-            if (dotIndex >= 0) {
-                extension = FNAME.substring(dotIndex + 1);
-            }
-            String result = map.getOrDefault(extension.toLowerCase(), "UNKNOWN");
-            System.out.println(result);
-        }
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    int N = in.nextInt();
+    int Q = in.nextInt();
+    HashMap<String, String> map = new HashMap<>();
+    for (int i = 0; i < N; i++) {
+      String EXT = in.next(); // file extension
+      String MT = in.next(); // MIME type.
+      map.putIfAbsent(EXT.toLowerCase(), MT);
     }
+    in.nextLine();
+    for (int i = 0; i < Q; i++) {
+      String FNAME = in.nextLine(); // One file name per line.
+      String extension = "";
+      int dotIndex = FNAME.lastIndexOf(".");
+      if (dotIndex >= 0) {
+        extension = FNAME.substring(dotIndex + 1);
+      }
+      String result = map.getOrDefault(extension.toLowerCase(), "UNKNOWN");
+      System.out.println(result);
+    }
+  }
 }
